a220d314af2a8ea5389551d87b58ec20b7b6b83f
Ismael Elias <ismael.esq@hotmail.com>
2019-04-19 14:55:56 -0300

    Finished method notOverlap. Add validation

--- src/Actions/Rules.js
+++ src/Actions/Rules.js
@@ -1,22 +1,22 @@
 const moment = require('moment')
 
 const notOverlap = (postInterval, jsonInterval) => {  
+  let returnFlag = true;
+  
   if(jsonInterval.length > 0){
     jsonInterval.forEach((jsonInterval) => {
-      console.log(postInterval, jsonInterval);
-      if(postInterval.start > jsonInterval.start || postInterval.start < jsonInterval.end && postInterval.end > jsonInterval.start || postInterval.end < jsonInterval.end){
-        console.log("falhou");
-        return false;
+      if(postInterval.start >= jsonInterval.start && postInterval.start <= jsonInterval.end || postInterval.end >= jsonInterval.start && postInterval.end <= jsonInterval.end){
+        returnFlag = false;
       }
-      console.log("passou do if");
-      return true;
     }); 
-  }else{
-    return true; 
   }
+  
+  return returnFlag
 }
 
 const DailyRule = (allRules, postInterval) => {
+  let error
+
   allRules.rules.forEach((rule) => {
     if(notOverlap(postInterval, rule.interval)){
       rule.interval.push(
@@ -27,69 +27,94 @@ const DailyRule = (allRules, postInterval) => {
           end: postInterval.end
         }
       )  
+    }else{
+      error = postInterval;
     }
   })
+
+  if(error){
+    throw new Error("Esse intervalo: { start: '"+ error.start +"', end: '"+ error.end +"'} já está preenchido. Selecione outro");
+  }
 
   return allRules
 }
 
 const WeeklyRule = (allRules, day, postInterval) => {
-    day.forEach((eachDay) => {
-      if(notOverlap(postInterval, allRules.rules[eachDay].interval)){
-        allRules.rules[eachDay].interval.push({
-          id: Math.floor((1 + Math.random()) * 0x10000),
-          date: null,
-          start: postInterval.start,
-          end: postInterval.end
-        })
-      }
-    })
-  
-    return allRules
+  let error
+
+  day.forEach((eachDay) => {
+    if(notOverlap(postInterval, allRules.rules[eachDay].interval)){
+      allRules.rules[eachDay].interval.push({
+        id: Math.floor((1 + Math.random()) * 0x10000),
+        date: null,
+        start: postInterval.start,
+        end: postInterval.end
+      })
+    }else{
+      error = postInterval;
+    }
+  })
+
+  if(error){
+    throw new Error("Esse intervalo: { start: '"+ error.start +"', end: '"+ error.end +"'} já está preenchido. Selecione outro");
+  }
+
+  return allRules
 }
 
 const SpecificDayRule = (allRules, day, postInterval) => {
-    postInterval.forEach((eachInterval) => {
-      if(notOverlap(postInterval, allRules.rules[moment(day).day()].interval)){
-        allRules.rules[moment(day).day()].interval.push({
-          id: Math.floor((1 + Math.random()) * 0x10000),
-          date: day,
-          start: eachInterval.start,
-          end: eachInterval.end
-        })
-      }
-    })
-  
-    return allRules
+  let error
+  postInterval.forEach((eachInterval) => {
+    if(notOverlap(eachInterval, allRules.rules[moment(day).day()].interval)){
+      allRules.rules[moment(day).day()].interval.push({
+        id: Math.floor((1 + Math.random()) * 0x10000),
+        date: day,
+        start: eachInterval.start,
+        end: eachInterval.end
+      })
+    }else{
+      error = eachInterval;
+    }
+  })
+
+  if(error){
+    throw new Error("Esse intervalo: { start: '"+ error.start +"', end: '"+ error.end +"'} já está preenchido. Selecione outro");
+  }
+
+  return allRules
 }
 
 const DeleteRule = (allRules, ruleId) => {
-    allRules.rules.forEach((rule) => {      
-      rule.interval.forEach((interval, Index) => {
-        if (interval.id === parseInt(ruleId)) {
-          rule.interval.splice(Index, 1)
-        }
-      })
+
+  allRules.rules.forEach((rule) => {      
+    rule.interval.forEach((interval, Index) => {
+      if (interval.id === parseInt(ruleId)) {
+        rule.interval.splice(Index, 1)
+      }
     })
-  
-    return allRules
+  })
+
+  return allRules
 }
 
 const AddRule = (allRules, typeRule, day, postInterval) => {
-    let rulesToReturn
-    if (typeRule === 'daily') {
-      rulesToReturn = DailyRule(allRules, postInterval)
-    }
-  
-    if (typeRule === 'weekly') {
+  let rulesToReturn
+
+  switch (typeRule) {
+    case 'daily':
+      rulesToReturn = DailyRule(allRules, postInterval)  
+      break;
+    case 'weekly':
       rulesToReturn = WeeklyRule(allRules, day, postInterval)
-    }
-  
-    if (typeRule === 'specificDay') {
+      break;
+    case 'specificDay':
       rulesToReturn = SpecificDayRule(allRules, day, postInterval)
-    }
-  
-    return rulesToReturn
+      break;
+    default:
+      throw new Error("Variável TypeRule com valor inválido");
+  }
+
+  return rulesToReturn
 }
 
 module.exports = {
